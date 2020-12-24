8bbc424e5f87a89f20273c49335dad2c8f5a3ae0
Basant kumar Hota <basahota@users.noreply.github.com>
2020-12-24 21:29:46 +0530

    Add files via upload

---
+++ src/test/java/com/javatechie/jib/GoogleJibExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.jib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleJibExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
